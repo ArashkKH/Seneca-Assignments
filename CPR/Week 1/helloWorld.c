51631fa7bcaac0633f1066f59e7b2a522d74715a
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-01-13 15:02:07 -0500

    CPR added

---
+++ CPR/Week 1/helloWorld.c
@@ -0,0 +1,19 @@
+/* Thanks to Brian Kernighan, 'Hello World' is the traditional first C program. It became
+legendary with the publication of "The C Programming Language" by Kernighan and Ritchie (1978).
+Now, Hello World is the canonical test of any programming language. */
+/*
+Full Name : Arash Kheirollahi
+Student ID# : 146731245
+UserID / Email : akheirollahi@myseneca.ca
+*/
+#include <stdio.h> // C language module providing Input/Output facilities
+int main(void) // main() is automatically called to start a C program
+{
+printf("Hello, World!\n");
+printf("\nMy name is: Arash Kheirollahi\n");
+printf("\nMy Student ID is: 146731245\n");
+printf("\nMy Email is: akheirollahi@myseneca.ca\n");
+// Why I came to Seneca ...
+/* insert printf statements here using code completion */
+return 0; // return to operating system
+}
