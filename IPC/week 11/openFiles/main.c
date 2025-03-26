c67fbdcc96a906da6a65c6a820782b71d204b7f1
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-03-26 00:09:24 -0700

    opening files for fun

---
+++ IPC/week 11/openFiles/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    FILE *testFile = NULL;
+
+    testFile = fopen("../test.txt" , "w");
+
+    if (testFile != NULL){
+        printf("that did work");
+    }else{
+        printf("that didnt work");
+    }
+
+
+}
